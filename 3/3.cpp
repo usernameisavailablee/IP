191c732a82e1bd26dde1985fb56aa3e5d2fa7ad8
Michael <91434795+usernameisavailablee@users.noreply.github.com>
2022-09-22 19:14:52 +0300

    3-я нулевая

---
+++ 3/3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void delete_twice (string &message)
+{
+    for (int i = 0; i < message.size(); i++)
+    {
+        if (message[i] == message[i + 1]) message.erase(i, 2);
+    }
+}
+bool have_twice(string message)
+{
+    bool a = false;
+    for (int i = 0; i < message.size(); i++)
+    {
+        if (message[i] == message[i + 1]) a=1;
+    }
+    return a;
+}
+int main()
+{
+    string message;
+    char now;
+    bool have;
+    cin >> message;
+    while (have_twice(message) != false)
+    {
+        delete_twice(message);
+    }
+
+    cout << message;
+}
