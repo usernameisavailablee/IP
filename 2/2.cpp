00c185959d7f0090990ea7f2d4ec0936d0beebf4
Michael <91434795+usernameisavailablee@users.noreply.github.com>
2022-09-22 16:24:11 +0300

    Нулевая 2-я

---
+++ 2/2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	/*Нулевая*/
+	//string s="", m = "", a = "";
+	//cin >> s; 
+	//int count=1;
+	//char now = s[0];
+	//for (int i = 1; i <= s.size(); i++)
+	//{
+	//	if (now != s[i])
+	//	{
+	//		if (count > 1) {
+	//			a = to_string(count);
+	//			m = m + a + now;
+	//			now = s[i];
+	//			count = 1;
+	//		}
+	//		else
+	//		{
+	//			m = m + now;
+	//			now = s[i];
+	//			count = 1;
+
+	//		}
+	//	}
+	//	else
+	//	{
+	//		count++;
+	//	}
+	//}
+	//cout << m;
+}
